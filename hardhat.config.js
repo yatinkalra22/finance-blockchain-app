bc13bbe626fcbd44124e99e48616f26081fd5915
Yatin Kalra <ykalra@presidio.com>
2023-10-01 16:23:14 +0530

    deploed contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
